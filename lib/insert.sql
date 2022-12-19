29f7dce4d4a49786b9a35fdbae52f59fd05e9106
Andrew Fairbairn <asfairbairn4@gmail.com>
2022-12-19 12:18:37 -0600

    commit tree d2cf67a29349293aa6c2cd92e2c595368c03787b

---
+++ lib/insert.sql
@@ -0,0 +1,15 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 10, 'M', 'Brown', 'Kind', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 5, 'M', 'Black', 'Aggressive', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha', 3, 'F', 'Brown', 'Ornery', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', 15, 'M', 'Black', 'Authoritative', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', 8, 'F', 'Black', 'Motherly', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', 6, 'M', 'Brown', 'Grumpy', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', 3, 'F', 'Brown', 'Rude', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 0, 'F', 'Black', 'Dependant', 1);
